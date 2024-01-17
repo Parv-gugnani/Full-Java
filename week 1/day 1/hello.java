0ed22af39c54b9d7dfd7902f494ebdf03bfefa04
Parv-gugnani <9765.parv@gmail.com>
2024-01-17 23:28:48 +0530

    day 1 finished

--- week 1/day 1/hello.java
+++ week 1/day 1/hello.java
@@ -1,2 +1,16 @@
-// intro
-class Main{}
+//day 1 intro
+class Main {
+    public static void main(String args[]) {
+        System.out.println(10 + 20);//PRINTED AS INTEGERS
+        // ln moves to another line
+        System.out.println("Hello");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("B");
+        System.out.println("B");
+        System.out.println("B");
+        System.out.println("B");
+        System.out.println("10 +20");//PRINTED AS STRING
+
+    }
+}
