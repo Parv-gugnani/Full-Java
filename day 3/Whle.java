930aa99057830ddd7de9c17a267b42eaa0e605ca
Parv-gugnani <9765.parv@gmail.com>
2024-01-22 23:24:55 +0530

    day 3 finished

--- day 3/Whle.java
+++ day 3/Whle.java
@@ -5,6 +5,17 @@ public class Whle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Q 
+        // System.out.print("Enter a number: ");
+        // int num = scanner.nextInt();
+        // int count = 4;
+        // while (count*count <= num) {
+        //     System.out.println(count*count + "");
+        //     count = count + 1;
+        // }
+
+
+
         // Q 1
         // System.out.print("Enter a number: ");
         // int num = scanner.nextInt();
@@ -28,5 +39,14 @@ public class Whle {
         // scanner.close();
 
         // Q3
+        // int i =1;
+        // while (i<=30) {
+        //     int result=i*i;
+        //     System.out.println(result+"");
+        //     i = i+1;
+            
+        // }
+
+        // Q4
     }
 }
