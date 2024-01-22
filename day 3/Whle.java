36fc46fb0bd7795876db04652f00abe04c281bad
Parv-gugnani <9765.parv@gmail.com>
2024-01-22 22:14:03 +0530

    while

---
+++ day 3/Whle.java
@@ -0,0 +1,32 @@
+//while loop
+import java.util.*;
+
+public class Whle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Q 1
+        // System.out.print("Enter a number: ");
+        // int num = scanner.nextInt();
+        // int count = 4;
+        // while (count <= num) {
+        //     System.out.println(count + "");
+        //     count = count + 4;
+        // }
+
+        // scanner.close();
+
+        // Q2
+        // till the perfect sqaure near the current number 
+        // int num = scanner.nextInt();
+        // int count = 1;
+
+        // while((count*count)<=num){
+        //     System.out.println(count + " ");
+        //     count = count +1;
+        // }
+        // scanner.close();
+
+        // Q3
+    }
+}
