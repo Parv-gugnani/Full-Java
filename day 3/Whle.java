319274043fe1a5156677f25cbc93ec4f3981a784
Parv-gugnani <9765.parv@gmail.com>
2024-01-23 17:20:24 +0530

    assign complet

--- day 3/Whle.java
+++ day 3/Whle.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Whle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         // Q 
         // System.out.print("Enter a number: ");
@@ -48,5 +47,118 @@ public class Whle {
         // }
 
         // Q4
+
+
+        // assignment
+        //1 
+        //1-N
+        // Scanner scanner = new Scanner(System.in);
+
+        // // Prompt the user to enter a positive integer
+        // System.out.print("Enter a positive integer N: ");
+
+        // // Read the user input
+        // int N = scanner.nextInt();
+
+        // // Display natural numbers from 1 to N using a while loop
+        // System.out.print("Natural numbers from 1 to " + N + ": ");
+        // int i = 1;
+        // while (i <= N) {
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+
+
+        // n-1
+        
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // // Prompt the user to enter a positive integer
+        // System.out.print("Enter a positive integer N: ");
+
+        // // Read the user input
+        // int N = scanner.nextInt();
+
+        // // Display natural numbers from 1 to N using a while loop
+        // System.out.print("Natural numbers from 1 to " + N + ": ");
+        // int i = 1;
+        // while (i <= N) {
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+        //all odd i n
+
+        //   Scanner scanner = new Scanner(System.in);
+
+        // // Read the user input
+        // int N = scanner.nextInt();
+        // int i = 1;
+        // while (i <= N) {
+        //     System.out.print(i + " ");
+        //     i+=2;
+        // }
+
+        // scanner.close();
+       
+       
+    //   all even 
+
+        //   Scanner scanner = new Scanner(System.in);
+
+        // // Read the user input
+        // int N = scanner.nextInt();
+        // int i = 1;
+        // while (i <= N) {
+        //     System.out.print(i + " ");
+        //     i+=2;
+        // }
+
+        // scanner.close();
+        
+
+        // sum till N
+
+        //   Scanner scanner = new Scanner(System.in);
+
+        // // Read the user input
+        // int N = scanner.nextInt();
+
+        // int sum = 0;
+        // int i = 1;
+        // while (i <= N) {
+        //     sum += i;
+        //     i++;
+        // }
+
+        // System.out.println(sum);
+
+
+        // scanner.close();
+
+
+        //multiple of 4
+
+          Scanner scanner = new Scanner(System.in);
+
+        // Read the user input
+        int N = scanner.nextInt();
+        int i = 4;
+        while (i <= N) {
+           System.out.println(i + "");
+            i+=4;
+        }
+
+
+        scanner.close();
+
+
+
+
+
+        
+        
     }
 }
