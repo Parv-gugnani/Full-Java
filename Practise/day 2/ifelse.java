f347eea338c4a60c3800687700c6e62483ae702d
Parv-gugnani <9765.parv@gmail.com>
2024-02-02 17:18:07 +0530

    if else

--- Practise/day 2/ifelse.java
+++ Practise/day 2/ifelse.java
@@ -7,18 +7,34 @@ public class ifelse {
         
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // int c = scanner.nextInt();
 
-        // 
-        if ((a >= b && a <= c) || (a <= b && a >= c)) {
-            System.out.println(a);
-        } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
-            System.out.println(b);
-        } else {
-            System.out.println(c);
+        // // 
+        // if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        //     System.out.println(a);
+        // } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        //     System.out.println(b);
+        // } else {
+        //     System.out.println(c);
+        // }
+
+        //q 8 additional
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+
+
+       if(n%m == 0)
+       {
+           System.out.println(n/m);
         }
+        else if(n%m == 1)
+        {
+           System.out.println(0);
+
+       }
 
         scanner.close();
 
