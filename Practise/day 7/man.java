c98ee50ca097814dc6f7ba85b25bb53a930dcf6c
Parv-gugnani <9765.parv@gmail.com>
2024-02-07 22:30:57 +0530

    array list

--- Practise/day 7/man.java
+++ Practise/day 7/man.java
@@ -27,5 +27,38 @@ public class man {
     // System.out.print(list.get(2));
     // // System.out.print(list.get(-1));
 
+    // ArrayList<ArrayList<Integer>> list2D = 
+    // new ArrayList<>();
+
+    // //Basic operation
+
+    // ArrayList<Integer> list1 = new ArrayList<>();
+    // list1.add(10);
+    // list1.add(20);
+    // list1.add(30);
+
+    // list2D.add(list1);
+
+    // ArrayList<Integer> list2 = new ArrayList<>();
+    // list2.add(0);
+  
+    // list2D.add(list2);
+
+    // ArrayList<Integer> list3 = new ArrayList<>();
+    // list3.add(1);
+    // list3.add(2);
+    // list3.add(3);
+    // list3.add(4);
+  
+    // list2D.add(list3);
+
+    // System.out.println(list2D);
+    // System.out.println();
+    // System.out.println(list2D.get(2));
+
+    // System.out.println(list2D.get(2).get(2));
+
+    // System.out.println(list2D.get(1));
+
     
 }
