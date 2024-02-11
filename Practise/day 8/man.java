3688cfbd24ed2e2d4b7cec93dde8538e01ceaabd
Parv-gugnani <9765.parv@gmail.com>
2024-02-11 22:23:05 +0530

    fix

--- Practise/day 8/man.java
+++ Practise/day 8/man.java
@@ -159,59 +159,6 @@ public class man {
     //    }
     //  }
 
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
-    // hello from past
 
 
     //Q3 is palindrome
