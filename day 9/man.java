638edf4459f30a3b5ab37c6a06005321efbaf8d9
Parv-gugnani <9765.parv@gmail.com>
2024-02-12 21:56:15 +0530

    HashSet

---
+++ day 9/man.java
@@ -0,0 +1,94 @@
+//hashset
+import java.util.*;
+// 
+public class man {
+    public static void main(String[] args){
+        // Syntax of HashSet
+        // HashSet<DataType> setName = new HashSet<Type>();
+        //int
+        // it doesn't follow insertion order  or sorting order
+        // HashSet<Integer> hs = new HashSet<>();
+        //basics operations
+        
+        //add
+        // hs.add(95);
+        // hs.add(60);
+        // hs.add(75);
+        // hs.add(56);
+        // hs.add(60);
+        // hs.add(60);
+        // hs.add(60);
+        // this wont be added as it was already added
+        // this will get ignored
+        
+        // System.out.print(hs);
+
+        // searching an element
+
+        // System.out.println(hs.contains(0));
+        //if it is is there then it will give ture or it will false
+
+        // Remove
+        // hs.remove(60);
+        // System.out.println(hs);
+
+        //Size
+        // hs.add(null);
+        // System.out.println(hs);
+
+        //Iterate Over all the elements
+        // for each
+
+        // for(DataType name: sourceDS){
+
+        // }
+
+        // for(Integer num: hs){
+        //     System.out.println(num);
+        //     //we use this when we think we can add null value
+        // }
+
+        // for(int num: hs){
+        //     System.out.println(num);
+        //     //we use this when wwe are not sure about null
+        // }
+
+
+        //Arraylist vs Hashset
+
+        //arraylist :
+        // -- sequential order
+        // --
+        
+        //HashSet:
+        // --
+        // --
+
+
+        // Q1 given an int array as input 
+        // , its element to the hashset and return hasset
+            //inpout:1,2,1,2
+            //inpout:1,2
+
+        
+        
+        //Q2 given two hasset as an input and print their
+        // common elemt
+
+        //input: hs1: 1,3,5,6
+        //input: hs2: 2,3,4,6
+
+        //output: 3 6
+   
+
+
+        
+     
+
+
+
+        
+
+    }
+    
+}
