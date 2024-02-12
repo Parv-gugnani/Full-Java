551fa0fde38a48fe327940b7d784bd9de0a352dc
Parv-gugnani <9765.parv@gmail.com>
2024-02-12 22:37:49 +0530

    hasmap

--- day 9/man.java
+++ day 9/man.java
@@ -1,8 +1,20 @@
 //hashset
 import java.util.*;
 // 
-public class man {
-    public static void main(String[] args){
+// public class man {
+
+
+
+// // Q2
+//     private static void pinter(HashSet<Integer> hs1,HashSet<Integer> hs2){
+//         for(int num:hs1){
+//             // num
+//             if(hs2.contains(num)==true){
+//                 System.out.println(num);
+//             }
+//         }
+//     }
+//     public static void main(String[] args){
         // Syntax of HashSet
         // HashSet<DataType> setName = new HashSet<Type>();
         //int
@@ -79,16 +91,99 @@ public class man {
         //input: hs2: 2,3,4,6
 
         //output: 3 6
-   
+
+        // int[] arr = {3,4,5,6};
 
 
+        // HashSet<Integer> hs1 = new HashSet<>();
+        // HashSet<Integer> hs2 = new HashSet<>();
+
+        // hs1.add(10);
+        // hs1.add(12);
+        // hs1.add(11);
+        // hs1.add(10);
+
+        // hs2.add(1);
+        // hs2.add(2);
+        // hs2.add(12);
+        // hs2.add(2);
+
+        // pinter(hs1,hs2);
+
+
+//     }
+    
+// }
+
+
+
+
+ 
+public class man {
+
+    public static void main(String[] args){
+
+        //Hashmap<Keytype>,valuetype> name = new Hashmap<>();
+        HashMap<String,Integer> map = new HashMap<>();
+
+        //basics operations
+
+        //inserrt-----------
+
+        map.put("Delhi",10);
+        map.put("Haryana",15);
+        map.put("Goa",10);
+        map.put("Haryana",25);
+
+        System.out.println(map);
         
-     
-
-
-
+        // no order same as it you put
         
+        //check if key is present or not--------
+        // System.out.println(map.containsKey("Delhi"));
+        //checking
 
-    }
+        //fetch coprresponding value associated with key
+        // .get(key)
+
+        // System.out.println(map.get("Delhi"));
+        // // 
+        // System.out.println(map.get("ABC"));
+        
+        
+        //update the value: put()
+        
+        // map.put("Delhi",15);
+        // System.out.println("thi is is after update"+map);
+
+
+        // System.out.println(map.size());
+
+
+        //remove:
+        // map.remove("Delhi");
+        // map.remove("Del");
+        // System.out.println(map);
+
+        //all the keys printing
+
+        // System.out.println(map.keySet());
+
+        // for(String str:map.keySet()){
+        //     System.out.println("State = "+ str);
+        // }
+
+        // Q1
+        //given an array as inpuyt return the corresponding frequncey  map
+
+        //input: 1,4,3,-2,1,0,2,1,3,4
+
+        //output: mainly to tell that how many timne occurance
+
+
+
+
+
+}
     
 }
