6d56d15c5342d31ed6e57732a3a0a77ea0f8cd4a
Parv-gugnani <9765.parv@gmail.com>
2024-02-21 21:53:16 +0530

    module 2

---
+++ module 2/day 1/man.java
@@ -0,0 +1,15 @@
+//DSA : intro to problem solving
+import java.util.*;
+
+public class man {
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+
+        
+    }
+}
