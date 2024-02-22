9ee68874d282929e4f2881ae3f2e19a5cd0437b7
Parv-gugnani <9765.parv@gmail.com>
2024-02-22 15:14:26 +0530

    assignment

--- module 2/day 1/man.java
+++ module 2/day 1/man.java
@@ -6,6 +6,29 @@ public class man {
 
         
         Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+
+      //Assignment
+
+      //Q 1
+    //   public class Solution {
+    //     public int solve(int A) {
+    
+    //          int count = 0;
+            
+    //         for(int i=1;i<=A;i++)
+    //         {
+    //            if (A % i == 0) {
+    //                 count++;
+    //             }
+    //         }
+    //         return count;
+    //     }
+    // }
+    
+
+
 
 
         scanner.close();
