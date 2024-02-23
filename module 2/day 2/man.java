e61883eb032547200b28687e377da27832033a06
Parv-gugnani <9765.parv@gmail.com>
2024-02-23 21:08:53 +0530

    time complex

---
+++ module 2/day 2/man.java
@@ -0,0 +1,13 @@
+//DSA : Time Complexity
+import java.util.*;
+
+public class man {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+
+
+        scanner.close();
+    }
+}
