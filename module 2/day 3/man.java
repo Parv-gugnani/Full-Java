9acac91462549f18d0901eb0002967066a701b48
Parv-gugnani <9765.parv@gmail.com>
2024-02-29 14:47:59 +0530

    dsa

--- module 2/day 3/man.java
+++ module 2/day 3/man.java
@@ -1,6 +1,6 @@
 //Space Complexity assignment
 
-public class Solution {
+public class man {
     public int solve(ArrayList<Integer> A) {
         if (A.isEmpty()) {
             return 0;
@@ -23,3 +23,74 @@ public class Solution {
         return sum;
     }
 }
+
+
+// public class Solution {
+ 
+//     private int cntIndexesToMakeBalance(int arr[], int n)
+//     {
+//         if (n == 1) {
+//             return 1;
+//         }
+     
+//         if (n == 2)
+//             return 0;
+     
+//         int sumEven = 0;
+//         int sumOdd = 0;
+     
+//         for (int i = 0; i < n; i++) {
+//             if (i % 2 == 0) {
+//                 sumEven += arr[i];
+//             }
+//             else {
+//                 sumOdd += arr[i];
+//             }
+//         }
+     
+//         int currOdd = 0;
+//         int currEven = arr[0];
+//         int res = 0;
+//         int newEvenSum = 0;
+//         int newOddSum = 0;
+     
+//         for (int i = 1; i < n - 1; i++) {
+//             if (i % 2 == 1) {
+//                 currOdd += arr[i];
+//                 newEvenSum = currEven + sumOdd
+//                              - currOdd;
+//                 newOddSum = currOdd + sumEven
+//                             - currEven - arr[i];
+//             }
+//             else {
+//                 currEven += arr[i];
+//                 newOddSum = currOdd + sumEven
+//                             - currEven;
+//                 newEvenSum = currEven + sumOdd
+//                              - currOdd - arr[i];
+//             }
+//             if (newEvenSum == newOddSum) {
+//                 res++;
+//             }
+//         }
+//         if (sumOdd == sumEven - arr[0]) {
+//             res++;
+//         }
+//         if (n % 2 == 1) {
+//             if (sumOdd == sumEven - arr[n - 1]) {
+//                 res++;
+//             }
+//         }
+//         else {
+//             if (sumEven == sumOdd - arr[n - 1]) {
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+ 
+//     public int solve(int[] A) {
+//         int n = A.length;
+//         return cntIndexesToMakeBalance(A, n);
+//     }
+// }
