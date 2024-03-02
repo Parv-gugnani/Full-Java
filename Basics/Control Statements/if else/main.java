9c8c98cd8e522df7240d4bc2edaad2c1d64cd2ca
Parv-gugnani <9765.parv@gmail.com>
2024-03-02 14:16:00 +0530

    if else

---
+++ Basics/Control Statements/if else/main.java
@@ -0,0 +1,15 @@
+//this is whole about the if else
+public class main {
+
+    public static void main(String[] args){
+
+        /*
+           if(condition){
+            code
+           }
+         */
+
+    }
+
+    
+}
---
+++ Basics/README.md
@@ -0,0 +1,5 @@
+# This is basics of java
+
+## JavaTpoint Docs
+
+![alt text](image.png)
