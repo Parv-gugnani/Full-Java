70b7fd25c9d42c6b12d3d3f93a5aea7a5bb5f732
Parv-gugnani <9765.parv@gmail.com>
2024-03-03 12:50:35 +0530

    switch completed

--- Basics/Control Statements/switch/man.java
+++ Basics/Control Statements/switch/man.java
@@ -67,6 +67,133 @@ public class man {
     //Printing month of the given number  
     System.out.println(monthString);  
 
+    System.out.println("------------");
+
+    char ch='O';
+    switch (ch) {
+        case 'a':   
+            System.out.println("Vowel");  
+            break;  
+        case 'e':   
+            System.out.println("Vowel");  
+            break;  
+        case 'i':   
+            System.out.println("Vowel");  
+            break;  
+        case 'o':   
+            System.out.println("Vowel");  
+            break;  
+        case 'u':   
+            System.out.println("Vowel");  
+            break;  
+        case 'A':   
+            System.out.println("Vowel");  
+            break;  
+        case 'E':   
+            System.out.println("Vowel");  
+            break;  
+        case 'I':   
+            System.out.println("Vowel");  
+            break;  
+        case 'O':   
+            System.out.println("Vowel");  
+            break;  
+        case 'U':   
+            System.out.println("Vowel");  
+            break;  
+        default:   
+            System.out.println("Consonant");  
+    }
+    
+    System.out.println("------------");
+    
+
+
+     String levelString = "Expert";
+        int level = 0;
+
+        switch(levelString){
+            case"Beginner": level = 1; break;
+            case"Intermediate": level = 2; break;
+            case"Expert": level = 3; break;
+
+            default: level=0;
+            break;
+        }
+        System.out.println("Your level is :" + level);
+
+        System.out.println("------------");
+        
+        
+        char branch = 'C';
+        int collegeYear = 4;
+
+        switch( collegeYear )  
+        {  
+            case 1:  
+                System.out.println("English, Maths, Science");  
+                break;  
+            case 2:  
+                switch( branch )   
+                {  
+                    case 'C':  
+                        System.out.println("Operating System, Java, Data Structure");  
+                        break;  
+                    case 'E':  
+                        System.out.println("Micro processors, Logic switching theory");  
+                        break;  
+                    case 'M':  
+                        System.out.println("Drawing, Manufacturing Machines");  
+                        break;  
+                }  
+                break;  
+            case 3:  
+                switch( branch )   
+                {  
+                    case 'C':  
+                        System.out.println("Computer Organization, MultiMedia");  
+                        break;  
+                    case 'E':  
+                        System.out.println("Fundamentals of Logic Design, Microelectronics");  
+                        break;  
+                    case 'M':  
+                        System.out.println("Internal Combustion Engines, Mechanical Vibration");  
+                        break;  
+                }  
+                break;  
+            case 4:  
+                switch( branch )   
+                {  
+                    case 'C':  
+                        System.out.println("Data Communication and Networks, MultiMedia");  
+                        break;  
+                    case 'E':  
+                        System.out.println("Embedded System, Image Processing");  
+                        break;  
+                    case 'M':  
+                        System.out.println("Production Technology, Thermal Engineering");  
+                        break;  
+                }  
+                break;  
+            }
+
+            // Java Enum in Switch Statement
+
+
+            // Java Wrapper in Switch Statement
+
+            Integer age = 18;
+            switch (age) {
+                case (16):System.out.print("You are under 18");break;
+                case (18):System.out.print("You are okay to vote");break;
+                case (65):System.out.print("You are senior citizen");break;
+                default: System.out.println("Please give the valid age");break;
+                
+            }
+
+
+        
+
 
 
 
