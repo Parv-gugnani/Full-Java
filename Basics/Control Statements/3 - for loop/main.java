1676bc4fe332fe548835d1815ab2aef3c29cc5d7
Parv-gugnani <9765.parv@gmail.com>
2024-03-03 13:10:21 +0530

    for loop

---
+++ Basics/Control Statements/3 - for loop/main.java
@@ -0,0 +1,53 @@
+//For Loop
+public class main {
+    public static void main(String[] args){
+
+        //for loop
+        // for(initialization; condition; increment/decrement){    
+        //     //statement or code to be executed    
+        //     }    
+
+        //ex - 1
+
+        for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+        }
+        System.out.println("------------");
+        
+        //printing 1 - 10 one by one
+
+        for (int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 3; j++){
+                System.out.println(i+" "+j);
+            }
+        } 
+
+        System.out.println("------------");
+        
+
+
+        // praymid example
+
+        for (int i = 1; i <=5; i++){
+            for (int j = 1; j <=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+        System.out.println("------------");
+        
+
+        // Pyramid Example 2:
+        int term = 5;
+        for(int i=1;i<=term;i++){  
+            for(int j=term;j>=i;j--){  
+                    System.out.print("* ");  
+            }  
+            System.out.println();
+        }
+
+
+    }
+    
+}
