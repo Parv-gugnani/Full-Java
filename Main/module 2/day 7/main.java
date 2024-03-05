402353b9e53eace14a8f4a1d0e01a7d08ac2f843
Parv-gugnani <9765.parv@gmail.com>
2024-03-05 11:32:09 +0530

    max subarray

--- Main/module 2/day 7/main.java
+++ Main/module 2/day 7/main.java
@@ -1,1 +1,25 @@
 // Sliding window & Contribution technique
+
+
+import java.util.ArrayList;
+
+public class main {
+
+
+    // Q 1
+    // public int maxSubarray(int A, int B, ArrayList<Integer> C) {
+
+    //     int maxsum = 0;
+
+    //     for(int i = 0 ; i < A ;i++){
+    //         int sum = 0;
+    //         for(int j = i ; j<A;j++){
+    //             sum = sum + C.get(j);
+    //             if(sum<=B && sum > maxsum){
+    //                 maxsum = sum;
+    //             }
+    //         }
+    //     }
+    //     return maxsum;  
+    // }
+}
