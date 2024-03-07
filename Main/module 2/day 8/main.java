eef933270dc96275edfc17e3cd6748434c654c5a
Parv-gugnani <9765.parv@gmail.com>
2024-03-07 12:48:56 +0530

    2d ar

--- Main/module 2/day 8/main.java
+++ Main/module 2/day 8/main.java
@@ -1,10 +1,14 @@
 //2d matrix
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args){
+
+
+        //input of 2darr
 
         Scanner scanner = new Scanner(System.in);
 
@@ -13,19 +17,42 @@ public class main {
 
         int [][] arr = new int[row][col];
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j <col; j++) {
-                arr[i][j] = scanner.nextInt();
-            }
-        }
+        // for(int i = 0; i < row; i++){
+        //     for(int j = 0; j <col; j++) {
+        //         arr[i][j] = scanner.nextInt();
+        //     }
+        // }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // System.out.println("This is how it looks like: ");
         
+
+        // for (int i = 0; i < row; i++) {
+        //     for (int j = 0; j < col; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+
+        /// Q 1
+        // You are given a 2D integer matrix A, return a 1D integer array containing column-wise sums of original matrix.
+
+        // int rows = A.size();
+        // int cols = A.get(0).size();
+        // ArrayList<Integer> columnSums = new ArrayList<>();
+
+
+
+        // // Calculate column-wise sums
+        // for (int j = 0; j < cols; j++) {
+        //     int sum = 0;
+        //     for (int i = 0; i < rows; i++) {
+        //         sum += A.get(i).get(j);
+        //     }
+        //     columnSums.add(j, sum);
+        // }
+
+        // return columnSums;
 
 
         
