2ee4a36682404cc3bc44a76f8561137c18f33c86
Parv-gugnani <9765.parv@gmail.com>
2024-03-07 12:24:03 +0530

    2d matrix

--- Main/module 2/day 8/main.java
+++ Main/module 2/day 8/main.java
@@ -1,17 +1,36 @@
 //2d matrix
 
+import java.util.Scanner;
+
 public class main {
 
-    public static void main(String[] args, int mat, int n, int m){
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+
+        int [][] arr = new int[row][col];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j <col; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+
 
         
 
-        for(int row=0; row<n;row++){
-            int sum = 0;
-            for(col =0;col<m;col++){
-                sum += mat[row][col];
-            }
-        }
+      
         
     }
     
