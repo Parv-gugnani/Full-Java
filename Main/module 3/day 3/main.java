7e2da824f937869f9a528a96535301d851415d42
Parv-gugnani <9765.parv@gmail.com>
2024-03-12 09:52:57 +0530

    repeating element

--- Main/module 3/day 3/main.java
+++ Main/module 3/day 3/main.java
@@ -16,3 +16,31 @@ public class Main {
         return sum;
     }
 }
+
+
+// import java.util.ArrayList;
+// import java.util.HashMap;
+
+// public class Solution {
+//     public int solve(ArrayList<Integer> A) {
+//         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        
+//         for (int num : A) {
+//             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+//         }
+        
+//         for (int num : A) {
+//             int countGreaterThanP = 0;
+//             for (int otherNum : A) {
+//                 if (otherNum > num) {
+//                     countGreaterThanP++;
+//                 }
+//             }
+//             if (countGreaterThanP == num) {
+//                 return 1;
+//             }
+//         }
+    
+//         return -1;
+//     }
+// }
