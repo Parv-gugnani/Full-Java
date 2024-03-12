3efa1cae641e064a2e665cba36d9fb809dfb6540
Parv-gugnani <9765.parv@gmail.com>
2024-03-12 09:41:05 +0530

    sorting assignment

--- Main/module 3/day 3/main.java
+++ Main/module 3/day 3/main.java
@@ -1,6 +1,18 @@
-//sorting
+import java.util.ArrayList;
+import java.util.Collections;
 
-
-public class main {
-    
+public class Main {
+    public int solve(ArrayList<Integer> A) {
+        
+        Collections.sort(A);
+        
+        int sum = 0, ans = 0;
+        
+        for(int i=0;i<A.size();i++) 
+        {
+            sum += (A.size()-i)*A.get(i);
+        }
+        
+        return sum;
+    }
 }
