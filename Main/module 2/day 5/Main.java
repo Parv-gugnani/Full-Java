36fe17d589998208eeec1c5fce7dc97389f726ad
Parv-gugnani <9765.parv@gmail.com>
2024-03-27 14:07:27 +0530

    Prefix sum

--- Main/module 2/day 5/Main.java
+++ Main/module 2/day 5/Main.java
@@ -1,24 +1,34 @@
 //Prefix Sum
 import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
-    static void fillPrefixSum(int arr[], int n,
-     int prefixSum[])
-    {
-        prefixSum[0] = arr[0];
-        for (int i = 1; i < n; ++i)
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
-    }
-    public static void main(String[] args)
-    {
-        int arr[] = { 10, 4, 16, 20 };
-        int n = arr.length;
-        int prefixSum[] = new int[n];
-       
-          // Function call
-        fillPrefixSum(arr, n, prefixSum);
-        for (int i = 0; i < n; i++)
-            System.out.print(prefixSum[i] + " ");
-        System.out.println("");
-    }
+
+
+
+    // Q1
+    //add the sum
+    // static void fillprefix(int arr[],int n, int prefixsum[]){
+    //     prefixsum[0] = arr[0];
+        
+    //     for(int i = 1; i < n; ++i){
+    //         prefixsum[i] = prefixsum[i - 1] + arr[i];
+    //     }
+    // }
+
+    // public static void main(String[] args){
+    //     int arr[] = {10,20,30,40,50};
+    //     int n = arr.length;
+    //     int prefixsum[] = new int[n];
+
+    //     fillprefix(arr, n, prefixsum);
+
+    //     for(int i = 0 ; i < n ;i ++)
+    //         System.out.print(prefixsum[i] + " ");
+    
+    //         System.out.println("");
+            
+    // }
+
+
 }
