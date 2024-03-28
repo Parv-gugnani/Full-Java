3aa60fa7fd35d6bf543ce19f6d081e47bc810a21
Parv-gugnani <9765.parv@gmail.com>
2024-03-28 11:37:59 +0530

    last ele

--- Main/module 4/day 1/Main.java
+++ Main/module 4/day 1/Main.java
@@ -1,19 +1,37 @@
 public class Main {
-    public int maxSubArray(final int[] A) {
-        int cursum = 0;
-           int maxsum = Integer.MIN_VALUE;
+    // public int maxSubArray(final int[] A) {
+    //     int cursum = 0;
+    //        int maxsum = Integer.MIN_VALUE;
 
 
-            for(int i = 0 ; i< A.length ;i++){
-                cursum += A[i];
-                if(cursum > maxsum){
-                    maxsum = cursum;
-                }
-                if(cursum < 0){
-                    cursum =0;
+    //         for(int i = 0 ; i< A.length ;i++){
+    //             cursum += A[i];
+    //             if(cursum > maxsum){
+    //                 maxsum = cursum;
+    //             }
+    //             if(cursum < 0){
+    //                 cursum =0;
+    //             }
+    //         }
+    //             return maxsum;
+
+    // }
+
+
+    // add one to last element of the list array
+    public int[] plusOne(int[] A) {
+            int n = A.length;
+    
+            for (int i = n - 1; i >= 0; i--) {
+                if (A[i] < 9) {
+                    A[i]++;
+                    return A;
+                } else {
+                    A[i] = 0;
                 }
             }
-                return maxsum;
-
+            int[] result = new int[n + 1];
+            result[0] = 1;
+            return result;
     }
 }
