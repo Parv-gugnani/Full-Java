d2601311c02484eecb1e063121419677a71ac747
Parv-gugnani <9765.parv@gmail.com>
2024-03-28 12:48:30 +0530

    while

---
+++ Basics/Control Statements/4 - while loop/Solution.java
@@ -0,0 +1,23 @@
+
+
+public class Solution {
+
+
+    /* 
+    syntax:
+    while (condition){    
+     //code to be executed   
+     I ncrement / decrement statement  
+     }   
+    */
+
+    public static void main(String[] args) {  
+        int i=1;  
+        while(i<=10){  
+            System.out.println(i);  
+        i++;  
+        }  
+    }  
+    
+    
+}
