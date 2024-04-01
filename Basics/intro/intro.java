b1216575bf7d035b22e8ad61081d475fce0a194a
Parv-gugnani <9765.parv@gmail.com>
2024-04-01 17:13:03 +0530

    intro completed

---
+++ Basics/intro/intro.java
@@ -0,0 +1,117 @@
+package Basics.intro;
+
+class intro {
+    public static void main(String[] args) {
+        
+        System.out.print("Hello worlds");
+        
+            //  
+        // int
+        int speedLimit = 80;
+
+        //float
+        int a = 1;
+        float b = 2.5;
+        char c = 'F';
+        
+
+        //float
+        //The float data type is a single-precision 32-bit floating-point. 
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+
+        //byte
+        //data type can have values from -128 to 127
+        //used to save memory
+        byte range;
+        range = 124;
+        System.out.println(range);
+
+
+        //short type
+        // data type in Java can have values from -32768 to 32767
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);  // prints -200
+
+        //int type
+        // data type can have values from -231 to 231-1 
+
+        int inttype = -4250000;
+        System.out.println(range);  // print -4250000
+
+        //long
+        //data type can have values from -263 to 263-1 
+        long lrange = -42332200000L;
+        System.out.println(range);
+
+
+        //double type
+        //data type is a double-precision 64-bit floating-point.
+        	
+        double number = -42.3;
+        System.out.println(number);  // prints -42
+
+
+        //char type
+        //It's a 16-bit Unicode character.
+
+        char letter = '\u0051';
+        System.out.println(letter);  // prints Q
+
+
+        // TODO: Java Operator
+         //Operators in Java can be classified into 5 types:
+         
+         // Arithmetic Operators
+         /*
+         a + b = 17
+
+         a - b = 7 
+
+         a * b = 60
+
+         a / b = 2 
+
+         a % b = 2 
+          */
+
+         // Assignment Operators
+
+         /*
+          var using =: 4
+        var using +=: 8 
+        var using *=: 32
+          */
+
+         // Relational Operators
+         /*
+          // == operator
+           System.out.println(a == b);  // false
+       
+           // != operator
+           System.out.println(a != b);  // true
+       
+           // > operator
+           System.out.println(a > b);  // false
+       
+           // < operator
+           System.out.println(a < b);  // true
+       
+           // >= operator
+           System.out.println(a >= b);  // false
+       
+           // <= operator
+           System.out.println(a <= b);  // true
+          */
+
+         //done
+
+         //Input and output
+
+
+        
+    }
+    
+}
