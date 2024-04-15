9edf6945fbdf199063227f2cb73c45b4e8d6aa8f
Parv-gugnani <9765.parv@gmail.com>
2024-04-15 21:35:57 +0530

    back

--- Main/module 4/10 - backtracking/Solution.java
+++ Main/module 4/10 - backtracking/Solution.java
@@ -1,3 +1,30 @@
+import java.util.*;
+
 public class Solution {
+
+
+   // Q 1 generate parentheses
    
-}
+   
+   public String[] generateParenthesis(int A) {
+           List<String> result = new ArrayList<>();
+           generate(result, "", 0, 0, A);
+           return result.toArray(new String[0]);
+       }
+   
+       private void generate(List<String> result, String current, int open, int close, int max) {
+           if (current.length() == max * 2) {
+               result.add(current);
+               return;
+           }
+   
+           if (open < max) {
+               generate(result, current + "(", open + 1, close, max);
+           }
+   
+           if (close < open) {
+               generate(result, current + ")", open, close + 1, max);
+           }
+       }
+
+    }
