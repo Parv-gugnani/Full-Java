ae0bfa872fe79b6e77edb08a0abb58321256209c
Parv-gugnani <9765.parv@gmail.com>
2024-04-15 15:26:42 +0530

    backtracking

---
+++ Main/module 4/10 - backtracking/easy/backtrackingallsub.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class backtrackingallsub {
+
+    public static void findsub(int[] arr)
+    {
+        int n = arr.length;
+
+        for (int i = 0; i < (1 << n); i++) {
+
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    System.out.print(arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args, int n)
+    {
+        int[] arr = new int[n];
+        findsub(arr);
+    }
+}
