ea33f0972423a8b8851ee9ba4793109105e7c97a
Parv-gugnani <9765.parv@gmail.com>
2024-04-20 14:08:09 +0530

    maths

--- Main/module 4/12 - maths ii/excel column no/Solution.java
+++ Main/module 4/12 - maths ii/excel column no/Solution.java
@@ -1,12 +1,9 @@
 public class Solution {
     public int titleToNumber(String A) {
         int result = 0;
-        int n = A.length();
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < A.length(); i++) {
             char c = A.charAt(i);
-            int numericalValue = c - 'A' + 1;
-            result += numericalValue * Math.pow(26, n - i - 1);
+            result = result * 26 + (c - 'A' + 1);
         }
 
         return result;
