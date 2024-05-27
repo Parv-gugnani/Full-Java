4df1e08ee0f893115df5499d44698b0b09420ce6
Parv-gugnani <9765.parv@gmail.com>
2024-05-27 16:37:57 +0530

    linked list

---
+++ Classes/module 6/2 - linked list ii/swap node/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
