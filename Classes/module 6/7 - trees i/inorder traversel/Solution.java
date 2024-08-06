a14cc942fe0368107ab5587213a74c92d399a456
Parv-gugnani <9765.parv@gmail.com>
2024-08-06 12:57:57 +0530

    trees

--- Classes/module 6/7 - trees i/inorder traversel/Solution.java
+++ Classes/module 6/7 - trees i/inorder traversel/Solution.java
@@ -1,16 +1,4 @@
-/**
- * Definition for binary tree
- * class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) {
- *      val = x;
- *      left=null;
- *      right=null;
- *     }
- * }
- */
+
 public class Solution {
     public ArrayList<Integer> inorderTraversal(TreeNode A) {
          ArrayList<Integer> result = new ArrayList<>();
